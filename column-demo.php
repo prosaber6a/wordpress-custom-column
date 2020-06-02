7b167eeb0e88f74963642076057df37231ab7078
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-06-02 19:40:17 +0600

    plugin created

---
+++ column-demo.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Custom Column
+ * Plugin URI: http://saberhr.com
+ * Author: SaberHR
+ * Author URI: http://saberhr.com
+ * Description: WordPress Custom Column API
+ * Licence: GPLv2 or Later
+ * Text Domain: custom-column
+ * Domain Path: /languages/
+ */
