656dfa62f61b6ef8dd38086a23e2a78fc118111a
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-06-02 19:53:51 +0600

    unset column and change order of column

--- column-demo.php
+++ column-demo.php
@@ -10,3 +10,27 @@
  * Text Domain: custom-column
  * Domain Path: /languages/
  */
+
+
+function custom_column_load_textdomain() {
+	load_plugin_textdomain( 'custom-column', false, plugin_dir_url( __FILE__ ) . '/languages' );
+}
+
+add_action( 'plugins_loaded', 'custom_column_load_textdomain' );
+
+function custom_column_post_columns( $columns ) {
+	print_r($columns);
+	// remove column form all post list
+	unset($columns['tags']);
+	unset($columns['comments']);
+
+	// change column order
+	unset($columns['author']);
+	unset($columns['date']);
+
+	$columns['author'] = "Author";
+	$columns['date'] = "Date";
+	return $columns;
+}
+
+add_filter( 'manage_posts_columns', 'custom_column_post_columns' );
