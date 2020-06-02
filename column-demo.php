b3354267550a8087ce8b0ea88dd525f2a41bef1e
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-06-02 20:09:23 +0600

    Thumbnail column added

--- column-demo.php
+++ column-demo.php
@@ -31,15 +31,23 @@ function custom_column_post_columns( $columns ) {
 	$columns['date']   = "Date";
 
 	// Add new column
-	$columns['id'] = __( 'Post ID', 'custom-column' );
+	$columns['id']        = __( 'Post ID', 'custom-column' );
+	$columns['thumbnail'] = __( 'Thumbnail', 'custom-column' );
 
 	return $columns;
 }
 
 add_filter( 'manage_posts_columns', 'custom_column_post_columns' );
 
+// New column value
 function custom_column_post_column_data( $columns, $post_id ) {
-	echo $post_id;
+	if ( 'id' == $columns ) {
+		echo $post_id;
+	} elseif ('thumbnail' == $columns) {
+		$thumbnail = get_the_post_thumbnail($post_id, array(50, 50));
+		echo $thumbnail;
+	}
+
 }
 
 add_action( 'manage_posts_custom_column', 'custom_column_post_column_data', 10, 2 );
